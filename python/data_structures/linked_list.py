4e5f1d1b8132c590b788907d84d24e4389a635a2
Rachel Freeland <rachel.freeland1@gmail.com>
2022-05-01 22:14:57 -0500

    Commit to save and start a branch

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,19 +1,23 @@
-
 from dataclasses import dataclass
 
 
 class Node:
+    '''This class creates the individual nodes linked in a LinkedList'''
     def __init__(self, data, next = "None"):
         self.data = data
         self.next = next
 
 class LinkedList:
     """
-    Put docstring here
+    This class creates and initializes a LinkedList. It also includes some of
+    the more common methods associated with working with a LinkedList
     """
 
     def __init__(self):
         self.head = None
+
+    def __str__(self):
+        pass
 
     def insert(self, value):
         # method body here
