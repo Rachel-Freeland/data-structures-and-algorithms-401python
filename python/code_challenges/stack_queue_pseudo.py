27d221a5725feafca38c6469038390e3d072b701
Rachel Freeland <rachel.freeland2@gmail.com>
2022-05-03 03:47:01 -0500

    Started Code challenge 11

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,16 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+
+    def __init__(s):
+        s.inbox = Stack()
+        s.outbox = Stack()
+
+    def enqueue(s, value):
+        """Arguments: value -->
+        Inserts the `value` into the PseudoQueue using FIFO"""
+        s.inbox.push(value)
+
+    def dequeue(s):
+        """Arguments: none -->
+        Extracts a value from PseudoQueue using FIFO"""
