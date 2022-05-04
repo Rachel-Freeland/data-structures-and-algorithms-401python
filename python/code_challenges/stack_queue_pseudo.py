2a626991065ba7c9c9e5870fb13ab00b6148449a
Rachel Freeland <rachel.freeland1@gmail.com>
2022-05-04 01:30:08 -0500

    All but 1 test passing

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,17 +1,24 @@
+from data_structures.invalid_operation_error import InvalidOperationError
 from data_structures.stack import Stack
-
 
 class PseudoQueue:
 
-    def __init__(s):
-        s.inbox = Stack()
-        s.outbox = Stack()
+    def __init__(self):
+        self.inbox = Stack()
+        self.outbox = Stack()
 
-    def enqueue(s, value):
+    def enqueue(self, value):
         """Arguments: value -->
         Inserts the `value` into the PseudoQueue using FIFO"""
-        s.inbox.push(value)
+        self.inbox.push(value)
 
-    def dequeue(s):
+    def dequeue(self):
         """Arguments: none -->
         Extracts a value from PseudoQueue using FIFO"""
+        while self.outbox.is_empty:
+            if self.inbox.is_empty:
+                self.outbox.push(self.inbox.pop())
+                return self.outbox.pop()
+            else:
+                raise InvalidOperationError
+
