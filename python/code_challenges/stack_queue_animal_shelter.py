97bbbac4c784d9a4372bcf94c870f8ebd80ba38f
Rachel Freeland <rachel.freeland1@gmail.com>
2022-05-04 10:19:25 -0500

    groundwork laid

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -1,13 +1,19 @@
 from data_structures.queue import Queue
 
-
 class AnimalShelter:
     pass
 
+    def __init__(self):
+        pass
+
+    def enqueue(self):
+        pass
+
+    def dequeue(self):
+        pass
 
 class Dog:
     pass
-
 
 class Cat:
     pass
