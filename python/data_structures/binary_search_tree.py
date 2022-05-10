ceb0fba3043055cc7b32d99932837e24d26255b5
Rachel Freeland <rachel.freeland1@gmail.com>
2022-05-09 21:03:48 -0500

    BinarySearchTree and methods  and  updated

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,4 +1,4 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
@@ -6,24 +6,59 @@ class BinarySearchTree(BinaryTree):
     Creates a subclass of the Binary tree that has additional methods
     """
 
-    def __init__(self):
-        super()__init__()
-        pass
-
-    def add(self):
+    def add(self, value):
         """
         Arguments: value ->
         Returns: Nothing
         Adds a new node with that value in the correct location in the binary tree search
         """
-        # method body here
-        pass
 
-    def contains(self):
+        def walk(root, new_node):
+
+            # base case
+            if not root:
+                return
+
+            if new_node.value < root.value:
+                # go left
+                if root.left:
+                    walk(root.left, new_node)
+                else:
+                    root.left = new_node
+            else:
+                if root.right:
+                    walk(root.right, new_node)
+                else:
+                    root.right = new_node
+                pass
+
+        node_to_add = Node(value)  # local variable outside walk fx
+
+        if not self.root:
+            self.root = node_to_add
+            return
+
+        walk(self.root, node_to_add)
+
+    def contains(self, value):
         """
         Arguments: value ->
         Returns: boolean
         Indicates whether a value is in the tree
         """
-        # method body here
-        pass
+
+        def walk(root, value):
+            if root is None:
+                return False
+
+            if root.value == value:
+                return True
+            elif root.value > value:
+                return walk(root.left, value)
+            elif root.value < value:
+                return walk(root.right, value)
+            else:
+                return False
+
+        return walk(self.root, value)
+
