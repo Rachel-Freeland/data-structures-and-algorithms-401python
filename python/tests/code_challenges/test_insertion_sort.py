882f66bf1276dad1d4cd1760ed6e8e225e8c8da0
Rachel Freeland <rachel.freeland1@gmail.com>
2022-05-24 02:12:16 -0500

    Code challenge class 26 started

---
+++ python/tests/code_challenges/test_insertion_sort.py
@@ -0,0 +1,10 @@
+from python.code_challenges.insertion_sort import insertion_sort
+
+
+def test_input_array():
+    arr = [8, 4, 23, 42, 16, 15]
+    actual = insertion_sort(arr)
+    expected = [4, 8, 15, 16, 23, 42]
+    assert actual == expected
+
+
