07af2579c4857ab05f2cc1f6e1899b67d7fa02f2
Rachel Freeland <rachel.freeland1@gmail.com>
2022-06-08 20:27:16 -0500

    Finished hashtable data structure except for update vs insert

--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -6,8 +6,27 @@ def test_exists():
     assert Hashtable
 
 
-@pytest.mark.skip("TODO")
-def test_get_apple():
+def test_create_default():
+    ht = Hashtable()
+    actual = ht.size
+    expected = 1024
+    assert actual == expected
+
+
+def test_hash():
+    ht = Hashtable()
+    index = ht.hash("cat")
+    assert index in range(0, ht.size)
+
+
+def test_set_apple():
+    ht = Hashtable()
+    ht.set("fruit", "apple")
+    fruit_index = ht.hash("fruit")
+    actual = ht.buckets[fruit_index]
+    expected = ("fruit", "apple")
+    assert actual.head.value == expected
+
     hashtable = Hashtable()
     hashtable.set("apple", "Used for apple sauce")
     actual = hashtable.get("apple")
@@ -15,7 +34,48 @@ def test_get_apple():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+def test_get_apple():
+    ht = Hashtable()
+    ht.set("fruit", "apple")
+
+    actual = ht.get("fruit")
+    expected = "apple"
+    assert actual == expected
+
+
+def test_collisions():
+    ht = Hashtable()
+    ht.set("cat", "Josie")
+    ht.set("act", "A Contemporary Theater")
+    ht.set("tac", "Taco Tuesday")
+
+    assert ht.get("cat") == "Josie"
+    assert ht.get("act") == "A Contemporary Theater"
+    assert ht.get("tac") == "Taco Tuesday"
+
+
+def test_contains():
+    ht = Hashtable()
+    ht.set("cat", "Josie")
+    ht.set("dog", "Luna")
+    expected = False
+    actual = ht.contains("zebra")
+    assert actual == expected
+
+
+def test_keys():
+    ht = Hashtable()
+    ht.set("cat", "Josie")
+    ht.set("act", "A Contemporary Theater")
+    ht.set("tac", "Taco Tuesday")
+    ht.set("dog", "Luna")
+    ht.set("fruit", "apple")
+    actual = ht.keys()
+    expected = ["fruit", "tac", "act", "cat", "dog"]
+    assert actual == expected
+
+
+@pytest.mark.skip
 def test_internals():
     hashtable = Hashtable(1024)
     hashtable.set("ahmad", 30)
