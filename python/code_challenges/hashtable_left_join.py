dc0323bcaeb163ef0806e1b74bb582d9c1087784
Rachel Freeland <rachel.freeland1@gmail.com>
2022-06-20 10:59:52 -0500

    Left Join challenge completed, README and whiteboard also completed

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,18 @@
 from data_structures.hashtable import Hashtable
 
 
-def left_join():
-    pass
+def left_join(ht1: Hashtable, ht2: Hashtable):
+    """
+    Arguments:
+        ht1: as a hashtable
+        ht2: as a hashtable
+    Return:
+        a collection of the keys with their respective synonyms and antonyms
+    This method left-joins ht2 to ht1.
+    """
+    collection = []
+
+    for key in ht1.keys():
+        joined = [key, ht1.get(key), ht2.get(key)]
+        collection.append(joined)
+    return collection
